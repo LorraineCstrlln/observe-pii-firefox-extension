64a45f094557b168bc6c8acbe137b36ea2664171
Lorraine Gwen M. Castrillon <110805691+LorraineCstrlln@users.noreply.github.com>
2026-04-01 23:00:46 +0800

    Reapply "fix: multiclass label and resulting tokens"
    This reverts commit 484f5816b19ce417d99d4c194cbe324cb8c9eded.

--- extension/new/inference.js
+++ extension/new/inference.js
@@ -7,25 +7,42 @@ export async function runInference(session, tokenizer, text) {
     const allRawOffsets = [];
     let isTextContextual = false;
 
+    const TOKEN_THRESHOLD = 0.5;
+
     try {
         for (const chunk of chunks) {
             const results = await session.run(chunk.feeds);
             const logits1 = Array.from(results.logits_head1.data);
             const probsHead1 = softMax([logits1[0], logits1[1]]);
             
-            if (probsHead1[1] > 0.5) isTextContextual = true;
+            if (probsHead1[1] > 0.6) isTextContextual = true;
+
+            console.log(probsHead1[1])
 
             const contextualMask = Array.from(results.contextual_mask.data); 
             const logitsHead2 = Array.from(results.logits_head2.data);
-            const numLabels = 2;
+            const numLabels = 10;
 
             const chunkTokens = chunk.tokenStrings.map((token, i) => {
                 const start = i * numLabels;
                 const logits = logitsHead2.slice(start, start + numLabels);
+                const probs = softMax(logits);
+
+                // Multi-class logic
+                const piiProbs = probs.slice(1);                // Ignore Other (Class 0)
+                const maxPII = Math.max(...piiProbs);           // best PII score
+                const labelIndex = piiProbs.indexOf(maxPII) + 1 // shift back to rel index
+                
+                const isAttention = chunk.offsets[i][0] !== 0 || chunk.offsets[i][1] !== 0;
+                const isPII = maxPII > TOKEN_THRESHOLD;
+                const isContextual = isAttention && contextualMask[i] === 1 && isPII
+
                 return {
                     token: token,
-                    probs: softMax(logits),
-                    isContextual: contextualMask[i] === 1
+                    probs: probs,
+                    maxPII: maxPII,
+                    label: labelIndex,
+                    isContextual: isContextual
                 };
             });
 
@@ -33,7 +50,8 @@ export async function runInference(session, tokenizer, text) {
             allRawOffsets.push(...chunk.offsets);
         }
 
-        const finalMergedPII = mergeSubwords(allRawTokens, allRawOffsets);
+        const finalMergedPII = mergeSubwords(allRawTokens, allRawOffsets, TOKEN_THRESHOLD);
+        console.log(finalMergedPII)
 
         return {
             head1: isTextContextual ? true : false, // simplified return
